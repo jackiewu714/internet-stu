c65cc6fe81939d52987a4caa266916846dd85e21
zhilw <zhilw@cenboomh.com>
2018-03-21 15:15:33 +0800

    格式化代码

--- internet-tech-stu/src/main/java/com/cw/activemq/Receiver.java
+++ internet-tech-stu/src/main/java/com/cw/activemq/Receiver.java
@@ -2,9 +2,8 @@
  * @Title: Receiver.java
  * @Package com.cw.mq.test
  * @Description: 消息接收者
- * Copyright: Copyright (c) 2014 
+ * Copyright: Copyright (c) 2014
  * Company:YY Inc
- * 
  * @author WuLiangzhi
  * @date Aug 23, 2015 2:11:46 PM
  * @version V1.0
@@ -30,78 +29,77 @@ import org.slf4j.LoggerFactory;
  * @Description: 消息接收者
  * @author WuLiangzhi
  * @date Aug 23, 2015 2:11:46 PM
- * 
+ *
  */
 
 public class Receiver {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);
 
-	public static void main(String[] args) {
-		// ConnectionFactory : 连接工厂，JMS用它创建连接
-		ConnectionFactory connectionFactory;
-		// Connection : JMS客户端到JMS Provider的连接
-		Connection connection = null;
-		// Session : 一个发送或者接收消息的线程
-		Session session = null;
-		// Destination : 消息的目的地，消息发送给谁
-		Destination destination;
-		// MessageConsumer : 消费者，消息接收者
-		MessageConsumer consumer = null;
+    public static void main(String[] args) {
+        // ConnectionFactory : 连接工厂，JMS用它创建连接
+        ConnectionFactory connectionFactory;
+        // Connection : JMS客户端到JMS Provider的连接
+        Connection connection = null;
+        // Session : 一个发送或者接收消息的线程
+        Session session = null;
+        // Destination : 消息的目的地，消息发送给谁
+        Destination destination;
+        // MessageConsumer : 消费者，消息接收者
+        MessageConsumer consumer = null;
 
-		String brokerURL = "tcp://localhost:61616";
-		String queueName = "FirstQueue";
+        String brokerURL = "tcp://localhost:61616";
+        String queueName = "FirstQueue";
 
-		connectionFactory = new ActiveMQConnectionFactory(
-				ActiveMQConnection.DEFAULT_USER,
-				ActiveMQConnection.DEFAULT_PASSWORD, brokerURL);
+        connectionFactory = new ActiveMQConnectionFactory(
+                ActiveMQConnection.DEFAULT_USER,
+                ActiveMQConnection.DEFAULT_PASSWORD, brokerURL);
 
-		try {
-			// 从构造工厂得到连接对象
-			connection = connectionFactory.createConnection();
+        try {
+            // 从构造工厂得到连接对象
+            connection = connectionFactory.createConnection();
 
-			// 启动
-			connection.start();
+            // 启动
+            connection.start();
 
-			// 获取操作连接
-			session = connection.createSession(Boolean.TRUE,
-					Session.AUTO_ACKNOWLEDGE);
+            // 获取操作连接
+            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
 
-			// 获取消息目的地对象，注意 queueName是队列名称，需在ActiveMQ的console中配置
-			destination = session.createQueue(queueName);
+            // 获取消息目的地对象，注意 queueName是队列名称，需在ActiveMQ的console中配置
+            destination = session.createQueue(queueName);
 
-			// 创建消费者对象
-			consumer = session.createConsumer(destination);
+            // 创建消费者对象
+            consumer = session.createConsumer(destination);
 
-			while (true) {
-				TextMessage message = (TextMessage) consumer.receive(10000);
-				if (null != message) {
-					LOG.info("接收到消息: {}", message.getText());
-				} else {
+            while (true) {
+                TextMessage message = (TextMessage) consumer.receive(10000);
+                if (null != message) {
+                    LOG.info("接收到消息: {}", message.getText());
+                } else {
 //					break;
-					Thread.sleep(1000);
-				}
-			}
-		} catch (JMSException e) {
-			LOG.error(e.getMessage(), e);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-		} finally {
-			try {
-				if (consumer != null) {
-					consumer.close();
-				}
-				if (session != null) {
-					session.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (JMSException e) {
-				LOG.error(e.getMessage(), e);
-			}
-		}
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (JMSException e) {
+            LOG.error(e.getMessage(), e);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (consumer != null) {
+                    consumer.close();
+                }
+                if (session != null) {
+                    session.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (JMSException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
 
-	}
+    }
 
 }

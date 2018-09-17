d4abd337e0dc3a606c997b36067df24e9c179ee3
zhilw <zhilw@cenboomh.com>
2018-09-17 13:15:34 +0800

    JDK动态代理和cglib代理测试用例

---
+++ spring-stu/src/main/java/com/cw/spring/stu/aop/proxy/impl/UserManagerImpl.java
@@ -0,0 +1,20 @@
+package com.cw.spring.stu.aop.proxy.impl;
+
+import com.cw.spring.stu.aop.proxy.IUserManager;
+
+/**
+ * 用户接口实现类
+ * @author WuLiangzhi  2018/09/17 12:47
+ */
+public class UserManagerImpl implements IUserManager {
+
+    @Override
+    public void addUser(String id, String pwd) {
+        System.out.println(String.format("调用了 UserManagerImpl.addUser(...) 方法, id=%s, pwd=%s", id, pwd));
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        System.out.println(String.format("调用了 UserManagerImpl.deleteUser(...) 方法, id=%s", id));
+    }
+}

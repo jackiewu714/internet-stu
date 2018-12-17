73f2caf51547893cf206ef0914dbc91c80111f0e
wuliangzhi <wuliangzhi@rhtop.cn>
2018-12-17 23:06:27 +0800

    springboot 入门demo

---
+++ spring-boot-stu/src/main/java/com/cw/stu/springboot/SpringBootStuApplication.java
@@ -0,0 +1,14 @@
+package com.cw.stu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStuApplication.class, args);
+    }
+
+}
+

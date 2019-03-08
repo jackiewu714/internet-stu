9a56491bb449258da7ca3275599cc61d5c44f237
zhilw <zhilw@cenboomh.com>
2019-03-08 16:16:48 +0800

    修改mybatis生成oracle的sqlmap文件的插件代码，批量插入采用序列自动填充主键值(union方式)

--- codegenerator/src/main/java/com/codegen/platform/dao/mybatis/plugin/OracleBatchOperSupportPlugin.java
+++ codegenerator/src/main/java/com/codegen/platform/dao/mybatis/plugin/OracleBatchOperSupportPlugin.java
@@ -1,4 +1,4 @@
-package com.codegen.platform.dao.mybatis.plugin;
+package com.cw.stu.internet.tech.platform.dao.mybatis.plugin;
 
 import org.apache.ibatis.session.RowBounds;
 import org.mybatis.generator.api.IntrospectedColumn;
@@ -23,15 +23,27 @@ import java.util.TreeSet;
 public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 
 	@Override
-	public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+	public boolean sqlMapDocumentGenerated(Document document,
+										   IntrospectedTable introspectedTable) {
 		this.introspectedTable = introspectedTable;
+		//生成sqlmap中批量插入节点（insert all 方式）
 		this.sqlMapInsertListSelectiveElementGenerated(document.getRootElement(), introspectedTable);
+		//生成sqlmap中批量插入节点（union 方式）
+		this.sqlMapInsertListSelectiveByUnionElementGenerated(document.getRootElement(), introspectedTable);
 		return true;
 	}
 
+	/**
+	 * 生成Mapper接口中批量插入方法（insert all 方式）
+	 * @param _method				Method
+	 * @param interfaze				Interface
+	 * @param introspectedTable		IntrospectedTable
+	 * @return	boolean
+	 */
 	@Override
-	public boolean clientInsertSelectiveMethodGenerated(Method _method, Interface interfaze,
-			IntrospectedTable introspectedTable) {
+	public boolean clientInsertSelectiveMethodGenerated(Method _method,
+														Interface interfaze,
+														IntrospectedTable introspectedTable) {
 
 		FullyQualifiedJavaType parameterType = introspectedTable.getRules().calculateAllFieldsClass();
 
@@ -52,7 +64,48 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
 
 		method.setName("insertListSelective");
-//		method.addParameter(new Parameter(parameterType, "record", "@Param(\"record\")"));
+		method.addParameter(new Parameter(listParameterType, "records", "@Param(\"records\")"));
+
+		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+		interfaze.addImportedTypes(importedTypes);
+		addMapperAnnotations(interfaze, method);
+
+		//生成Mapper接口中批量插入方法（union 方式）
+		this.clientInsertSelectiveByUnionMethodGenerated(_method, interfaze, introspectedTable);
+		return true;
+	}
+
+	/**
+	 * 生成Mapper接口中批量插入方法（union 方式）
+	 * @param _method				Method
+	 * @param interfaze				Interface
+	 * @param introspectedTable		IntrospectedTable
+	 * @return	boolean
+	 */
+	public boolean clientInsertSelectiveByUnionMethodGenerated(Method _method,
+															   Interface interfaze,
+															   IntrospectedTable introspectedTable) {
+
+		FullyQualifiedJavaType parameterType = introspectedTable.getRules().calculateAllFieldsClass();
+
+		Method method = new Method();
+		BeanUtils.copyProperties(_method, method);
+		this.introspectedTable = introspectedTable;
+		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
+		importedTypes.add(parameterType);
+		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+
+		method.setVisibility(JavaVisibility.PUBLIC);
+
+		FullyQualifiedJavaType listParameterType = FullyQualifiedJavaType.getNewListInstance();
+		FullyQualifiedJavaType rowBoundsType = new FullyQualifiedJavaType(RowBounds.class.getName());
+		importedTypes.add(rowBoundsType);
+
+		listParameterType.addTypeArgument(parameterType);
+		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+
+		method.setName("insertListSelectiveByUnion");
+		method.addParameter(new Parameter(parameterType, "record", "@Param(\"record\")"));
 		method.addParameter(new Parameter(listParameterType, "records", "@Param(\"records\")"));
 
 		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
@@ -61,12 +114,20 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 		return true;
 	}
 
-	public void addMapperAnnotations(Interface interfaze, Method method) {
+	public void addMapperAnnotations(Interface interfaze,
+									 Method method) {
 		interfaze.addMethod(method);
 		return;
 	}
 
-	public boolean sqlMapInsertListSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+	/**
+	 * 生成sqlmap中批量插入节点（insert all 方式）
+	 * @param element				XmlElement
+	 * @param introspectedTable		IntrospectedTable
+	 * @return	boolean
+	 */
+	public boolean sqlMapInsertListSelectiveElementGenerated(XmlElement element,
+															 IntrospectedTable introspectedTable) {
 		this.introspectedTable = introspectedTable;
 
 		XmlElement answer = new XmlElement("insert");
@@ -77,15 +138,13 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 
 		context.getCommentGenerator().addComment(answer);
 
-		StringBuilder sb = new StringBuilder();
-
-//		genInsertXml(introspectedTable, answer, sb);
-
-		genInsertAllXml(introspectedTable, answer, sb);
+		genInsertAllXml(introspectedTable, answer);
 		return true;
 	}
 
-	public void genInsertXml(IntrospectedTable introspectedTable, XmlElement answer, StringBuilder sb) {
+	public void genInsertXml(IntrospectedTable introspectedTable,
+							 XmlElement answer,
+							 StringBuilder sb) {
 		XmlElement insertTrimElement = new XmlElement("trim");
 		insertTrimElement.addAttribute(new Attribute("prefix", "("));
 		insertTrimElement.addAttribute(new Attribute("suffix", ")"));
@@ -139,7 +198,7 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 	}
 
 	/**
-	 * 生成oracle批量插入的sqlmap语句块，示例代码如下：
+	 * 生成oracle批量插入的sqlmap语句块（insert all 方式），示例代码如下：
 	 *
 	 *  INSERT ALL
 	 *  	INTO bms.test(name1, name2, name3, name4, name5) VALUES(1, 12, 13, 14, 15)
@@ -149,10 +208,11 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 	 *
 	 * @param introspectedTable		IntrospectedTable
 	 * @param answer				XmlElement
-	 * @param sb					StringBuilder
 	 */
-	protected void genInsertAllXml(IntrospectedTable introspectedTable, XmlElement answer, StringBuilder sb) {
-		sb.append("insert all ");
+	protected void genInsertAllXml(IntrospectedTable introspectedTable,
+								   XmlElement answer) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT ALL ");
 		answer.addElement(new TextElement(sb.toString()));
 
 		XmlElement insertTrimElement = new XmlElement("trim");
@@ -203,13 +263,113 @@ public class OracleBatchOperSupportPlugin extends BasePluginAdapter {
 		}
 
 		String tableName = introspectedTable.getFullyQualifiedTableNameAtRuntime();
-		innerForEach.addElement(new TextElement(String.format("into %S", tableName)));
+		innerForEach.addElement(new TextElement(String.format("INTO %S", tableName)));
 		innerForEach.addElement(insertTrimElement);
-		innerForEach.addElement(new TextElement(" values "));
+		innerForEach.addElement(new TextElement(" VALUES "));
 		innerForEach.addElement(valuesTrimElement);
-		innerForEach.addElement(new TextElement(" SELECT 1 FROM DUAL "));
 
 		answer.addElement(innerForEach);
+		answer.addElement(new TextElement(" SELECT 1 FROM DUAL "));
+	}
+
+	/**
+	 * 生成sqlmap中批量插入节点（union 方式）
+	 * @param element				XmlElement
+	 * @param introspectedTable		IntrospectedTable
+	 * @return	boolean
+	 */
+	public boolean sqlMapInsertListSelectiveByUnionElementGenerated(XmlElement element,
+																	IntrospectedTable introspectedTable) {
+		this.introspectedTable = introspectedTable;
+
+		XmlElement answer = new XmlElement("insert");
+		answer.addAttribute(new Attribute("id", "insertListSelectiveByUnion"));
+		answer.addAttribute(new Attribute("parameterType", "map"));
+
+		element.addElement(answer);
+
+		context.getCommentGenerator().addComment(answer);
+
+		genInsertByUnionXml(introspectedTable, answer);
+		return true;
+	}
+
+	/**
+	 * 生成oracle批量插入的sqlmap语句块（union 方式），示例代码如下：
+	 *
+	 *  INSERT INTO bms.test(name1, name2, name3, name4, name5)
+	 *  	SELECT 1, 12, 13, 14, 15 FROM dual UNION ALL
+	 *  	SELECT 2, 22, 23, 24, 25 FROM dual UNION ALL
+	 *  	SELECT 3, 32, 33, 34, 35 FROM dual
+	 *
+	 * @param introspectedTable		IntrospectedTable
+	 * @param answer				XmlElement
+	 */
+	protected void genInsertByUnionXml(IntrospectedTable introspectedTable,
+									   XmlElement answer) {
+		XmlElement insertTrimElement = new XmlElement("trim");
+		insertTrimElement.addAttribute(new Attribute("prefix", "("));
+		insertTrimElement.addAttribute(new Attribute("suffix", ")"));
+		insertTrimElement.addAttribute(new Attribute("suffixOverrides", ","));
+
+		XmlElement valuesTrimElement = new XmlElement("trim");
+		valuesTrimElement.addAttribute(new Attribute("prefix", " "));
+		valuesTrimElement.addAttribute(new Attribute("suffix", " "));
+		valuesTrimElement.addAttribute(new Attribute("suffixOverrides", ","));
+
+		StringBuilder sb = new StringBuilder();
+		for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+			if (introspectedColumn.isIdentity()) {
+				// cannot set values on identity fields
+				// continue;
+			}
+			XmlElement insertNotNullElement = new XmlElement("if");
+			sb.setLength(0);
+			sb.append(introspectedColumn.getJavaProperty("record."));
+			sb.append(" != null");
+			insertNotNullElement.addAttribute(new Attribute("test", sb.toString()));
+
+			sb.setLength(0);
+			sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+			sb.append(',');
+			insertNotNullElement.addElement(new TextElement(sb.toString()));
+			insertTrimElement.addElement(insertNotNullElement);
+
+			//主键列，则不拼接了(采用序列方式生成主键ID的值)
+			if(introspectedColumn.isSequenceColumn()) {
+				continue;
+			}
+			XmlElement valuesNotNullElement = new XmlElement("if");
+			sb.setLength(0);
+			sb.append(introspectedColumn.getJavaProperty("record."));
+			sb.append(" != null");
+			valuesNotNullElement.addAttribute(new Attribute("test", sb.toString()));
+
+			sb.setLength(0);
+			sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+			sb.append(',');
+			valuesNotNullElement.addElement(new TextElement(sb.toString().replace("#{", "#{listItem.")));
+			valuesTrimElement.addElement(valuesNotNullElement);
+		}
+
+		String tableName = introspectedTable.getFullyQualifiedTableNameAtRuntime();
+		answer.addElement(new TextElement(String.format("INSERT INTO %S", tableName)));
+		answer.addElement(insertTrimElement);
+
+		XmlElement innerForEach = new XmlElement("foreach");
+		innerForEach.addAttribute(new Attribute("collection", "records"));
+		innerForEach.addAttribute(new Attribute("item", "listItem"));
+		innerForEach.addAttribute(new Attribute("open", " "));
+		innerForEach.addAttribute(new Attribute("close", " "));
+		innerForEach.addAttribute(new Attribute("separator", "union all"));
+
+		innerForEach.addElement(new TextElement("SELECT"));
+		innerForEach.addElement(valuesTrimElement);
+		innerForEach.addElement(new TextElement("FROM DUAL"));
+
+		answer.addElement(new TextElement(String.format(" SELECT %s_SEQ.NEXTVAL, A.* from( ", tableName)));
+		answer.addElement(innerForEach);
+		answer.addElement(new TextElement(" ) A "));
 	}
 
 	@Override

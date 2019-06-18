12e9bdd23b560e090466243dce1defbbb6c5024f
zhilw <zhilw@cenboomh.com>
2019-06-18 18:49:17 +0800

    spring boot 2 测试用例调试

---
+++ internet-tech-stu/src/main/java/study/javabase/string/StringTest2.java
@@ -0,0 +1,19 @@
+package study.javabase.string;
+
+import org.junit.Test;
+
+/**
+ * @author WuLiangzhi  2019/06/18 17:57
+ */
+public class StringTest2 {
+
+    @Test
+    public void testCompare(){
+        String str1 = "HA0105B00801191102";
+        String str2 = "HA0105B00801191102Z";
+
+        int comp = str1.compareTo(str2);
+        System.out.println("comp=" + comp);
+    }
+
+}

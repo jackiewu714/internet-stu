2b95d5396078b6e621d816c5e833e874534205f1
zhilw <zhilw@cenboomh.com>
2019-07-16 11:11:49 +0800

    netty study - hello world

--- netty-stu/src/main/java/com/cw/stu/netty/helloworld/NettyHelloWorldServer.java
+++ netty-stu/src/main/java/com/cw/stu/netty/helloworld/NettyHelloWorldServer.java
@@ -1,9 +1,70 @@
 package com.cw.stu.netty.helloworld;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Netty Hello World Server
  *
  * @author WuLiangzhi  2019/07/14 10:53
  */
 public class NettyHelloWorldServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyHelloWorldServer.class);
+
+    public static void main(String[] args) {
+        int port = 9898;
+        new NettyHelloWorldServer().bind(port);
+    }
+
+    private void bind(int port) {
+
+        /*
+         * interface EventLoopGroup extends EventExecutorGroup extends ScheduledExecutorService extends ExecutorService
+         * 配置服务端的 NIO 线程池,用于网络事件处理，实质上他们就是 Reactor 线程组
+         * bossGroup 用于服务端接受客户端连接，workerGroup 用于进行 SocketChannel 网络读写
+         */
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChildChannelHandler());
+
+        try {
+            // 服务器启动辅助类配置完成后，调用 bind 方法绑定监听端口，调用 sync 方法同步等待绑定操作完成
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            System.out.println(Thread.currentThread().getName() + ",服务器开始监听端口，等待客户端连接......");
+
+            // 下面会进行阻塞，等待服务器连接关闭之后 main 方法退出，程序结束
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage(), e);
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new NettyHelloWorldServerHandler());
+        }
+    }
+
 }

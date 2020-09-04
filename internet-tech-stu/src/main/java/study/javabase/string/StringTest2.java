d8facb5d9a9f5322edd4cc0f615d8fe4dc49a55e
zhilw <zhilw@cenboomh.com>
2020-09-04 18:07:03 +0800

    string格式化

--- internet-tech-stu/src/main/java/study/javabase/string/StringTest2.java
+++ internet-tech-stu/src/main/java/study/javabase/string/StringTest2.java
@@ -2,6 +2,8 @@ package study.javabase.string;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 /**
  * @author WuLiangzhi  2019/06/18 17:57
  */
@@ -21,6 +23,33 @@ public class StringTest2 {
         String str = "11";
         String[] strArr = str.split(",");
         System.out.println("strArr=" + strArr);
+    }
+
+    @Test
+    public void testFormat(){
+        Double sum = 0.0;
+        sum = sum(sum, 0d);
+        String str = String.format ("%.2f", sum);
+        System.out.println("str=" + str);
+    }
+
+    /**
+     * double 相加
+     *
+     * @param d1 double
+     * @param d2 double
+     * @return double
+     */
+    public static double sum(Double d1, Double d2) {
+        if (d1 == null) {
+            d1 = 0d;
+        }
+        if (d2 == null) {
+            d2 = 0d;
+        }
+        final BigDecimal bd1 = new BigDecimal(Double.toString(d1));
+        final BigDecimal bd2 = new BigDecimal(Double.toString(d2));
+        return bd1.add(bd2).doubleValue();
     }
 
 }
